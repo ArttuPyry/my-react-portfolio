ae76bf9589edaee6927d612d66644854bf660b16
ArttuNipa <74288949+ArttuNipa@users.noreply.github.com>
2022-10-18 16:39:41 +0300

    Nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,13 @@ import './nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#skills">Skills</a>
+      <a href="#contact">Contact</a>
+    </nav>
   )
 }
 
